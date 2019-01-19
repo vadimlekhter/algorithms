5f3f9f99ba0416f6154811ece47bc193f81fb0bd
vadimlekhter <v_lehter@mail.ru>
2019-01-20 00:50:31 +0300

    lesson-1

---
+++ public/index.php
@@ -0,0 +1,71 @@
+<?php
+
+function a($dir)
+{
+    $dir = new DirectoryIterator($dir);
+
+    foreach ($dir as $item) {
+        if (!$item->isDot()) {
+            if ($item->isDir()) {
+                echo '<b>'.$item.'</b>' . '<br>';
+                a($item->getPathname());
+            } else {
+                echo $item . '<br>';
+            }
+        }
+    }
+}
+
+a("./");
+
+echo '--------------------------------------------<br><br><br>';
+
+
+
+$count = 1000000;
+$last = memory_get_usage();
+echo 'Операций:' . $count . '<br><br>';
+
+$begin = microtime();
+$arr = [];
+for ($i=0; $i < $count; $i++) {
+    $arr[] = $i;
+}
+echo 'Оператор for<br>';
+echo 'Время:   <br>';
+print_r(microtime() - $begin);
+echo "<br>";
+echo 'Память:   ';
+echo ((memory_get_usage() - $last)/1048576) . PHP_EOL;
+$last = memory_get_usage();
+
+echo "<br><br>";
+
+$j=0;
+$begin = microtime();
+foreach ($arr as $item) {
+    $j++;
+}
+echo 'Оператор foreach<br>';
+echo 'Время:   <br>';
+print_r(microtime() - $begin);
+echo "<br>";
+echo 'Память:   ';
+echo ((memory_get_usage() - $last)/1048576) . PHP_EOL;
+$last = memory_get_usage();
+
+echo "<br><br>";
+
+
+$begin = microtime();
+$obj = new ArrayIterator([]);
+for ($i=0; $i < 1000000; $i++) {
+    $obj->append($i);
+}
+echo 'Итератор<br>';
+echo 'Время:   <br>';
+print_r(microtime() - $begin);
+echo "<br>";
+echo 'Память:   ';
+echo ((memory_get_usage() - $last)/1048576) . PHP_EOL;
+$last = memory_get_usage();
